c626103292d5388cf44220713113e2b4b17c2b0f
Ben Chan <ben@chanhq.com>
2020-12-08 19:29:56 +0000

    Finish up basic search functionalilty using basic state

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ class App extends Component {
     };
   }
 
-  onSearchChange(e) {
-    console.log(e.target.value);
-  }
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
 
   render() {
+    const filterRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <CardList robots={filterRobots} />
       </div>
     );
   }
