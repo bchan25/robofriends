5981fbfda82e4a51f40b3591ad79c411392b046e
Ben Chan <ben@chanhq.com>
2020-12-08 18:54:37 +0000

    Optimise the looping in CardList

--- src/CardList.js
+++ src/CardList.js
@@ -4,13 +4,22 @@ import Card from './Card'
 
 const CardList = ({robots}) => {
 
-    const cardArray = robots.map((user, i) => {
-            return  <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    });
+ 
 
     return (
         <div>
-            {cardArray}
+            {
+                robots.map((user, i) => {
+                    return(
+                        <Card 
+                            key={i} 
+                            id={robots[i].id} 
+                            name={robots[i].name} 
+                            email={robots[i].email}
+                            />
+                    );
+                })
+            }
         </div>
     )
 }
