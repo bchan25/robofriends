0e962d57a804af0913c4574edee4fe5d850a3113
Ben Chan <ben@chanhq.com>
2020-12-08 19:17:22 +0000

    Added state into app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CardList from './CardList'
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
-import {robots} from './robots'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
