86d0a571b9d2703bb4f2cd8f0405584f67c6919d
Ben Chan <ben@chanhq.com>
2020-12-09 10:07:02 +0000

    Remove local json, Fetch user data from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (e) => {
@@ -23,13 +28,18 @@ class App extends Component {
         .toLocaleLowerCase()
         .includes(this.state.searchfield.toLocaleLowerCase());
     });
-    return (
-      <div className='tc'>
-        <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filterRobots} />
+        </div>
+      );
+    }
   }
 }
 
